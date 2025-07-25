5c39d019be2e7212e4f7f67e45d60f366d1a42f8
SibuCodeX <sibu25456@gmail.com>
2025-07-25 11:42:56 +0530

    .

---
+++ 43_implementing-login-with-google-on-backend-server/services/googleAuthService.js
@@ -0,0 +1,27 @@
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL;
+
+export async function fetchUserFromGoogle(code) {
+  console.log("Running fetchIdToken function...");
+  const payload = `code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URL}&grant_type=authorization_code`;
+
+  const response = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: payload,
+  });
+
+  const data = await response.json();
+  if (data.error) {
+    console.log("Error occurred");
+    console.log(data);
+    return;
+  }
+
+  const userToken = data.id_token.split(".")[1];
+  const userData = JSON.parse(atob(userToken));
+  return userData;
+}
