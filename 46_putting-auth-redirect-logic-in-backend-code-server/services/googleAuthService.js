f2fcf592556058a45a9f2ac192c544d0cbda52c8
SibuCodeX <sibu25456@gmail.com>
2025-07-26 13:58:38 +0530

    .

---
+++ 46_putting-auth-redirect-logic-in-backend-code-server/services/googleAuthService.js
@@ -0,0 +1,31 @@
+const clientId =
+  process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const redirectUrl = process.env.GOOGLE_REDIRECT_URL;
+
+export async function fetchUserFromGoogle(code) {
+  console.log("Running fetchIdToken function...");
+  const payload = `code=${code}&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUrl}&grant_type=authorization_code`;
+
+  const response = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: payload,
+  });
+
+  const data = await response.json();
+  if (data.error) {
+    console.log("Error occurred");
+    console.log(data);
+    return;
+  }
+
+  console.log(data.id_token);
+  const userToken = data.id_token.split(".")[1];
+  const userData = JSON.parse(atob(userToken));
+
+  console.log(userData);
+  return userData;
+}
