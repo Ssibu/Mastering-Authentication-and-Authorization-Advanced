47027e12b43f9631830ae892c5cd151062f749ce
SibuCodeX <sibu25456@gmail.com>
2025-07-26 16:11:49 +0530

    .

---
+++ 49_using-google-auth-library-to-fetch-token/services/googleAuthService.js
@@ -0,0 +1,27 @@
+import { OAuth2Client } from "google-auth-library";
+
+const clientId =
+  process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const redirectUrl = process.env.GOOGLE_REDIRECT_URL;
+
+const client = new OAuth2Client({
+  clientId,
+  clientSecret,
+  redirectUri: redirectUrl,
+});
+
+export async function fetchUserFromGoogle(code) {
+  console.log("Running fetchIdToken function...");
+
+  const { tokens } = await client.getToken(code);
+
+  const loginTicket = await client.verifyIdToken({
+    idToken: tokens.id_token,
+    audience: clientId,
+  });
+
+  const userData = loginTicket.getPayload();
+  console.log(userData);
+  return userData;
+}
