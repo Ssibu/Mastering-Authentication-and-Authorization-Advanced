3993bdcd30e61e3a47074605b1137621fcdc35dd
SibuCodeX <sibu25456@gmail.com>
2025-07-29 16:26:46 +0530

    .

--- 45_recieving-auth-code-on-backend-client/login/script.js
+++ 45_recieving-auth-code-on-backend-client/login/script.js
@@ -7,7 +7,7 @@ const redirectUrl = "http://localhost:4000/auth/google/callback";
 const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&scope=openid email profile&redirect_uri=${redirectUrl}`;
 
 button.addEventListener("click", () => {
-  window.open(
+  window.open( 
     "http://localhost:4000/auth/google",
     "auth-popup",
     "width=500,height=600"
